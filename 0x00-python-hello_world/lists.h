5c925a1aa8f4592ba3f990b44a8bb0f01853f096
BrownDesire <anosikesuccess0@gmail.com>
2023-01-02 14:29:40 -0800

    first

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,17 @@
+ISTS_H
+ISTS_H
+
+<stdlib.h>
+
+truct listint_s
+
+nt n;
+truct listint_s *nest;
+-t;
+
+int_listint(const listint_t *h);
+*add_nodeint(listint_t **head, const int n);
+_listint(listint_t *head);
+_cycle(listint_t *list);
+
+LISTS_H */
